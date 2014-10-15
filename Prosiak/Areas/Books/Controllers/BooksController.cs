649574a59c58adf891f474191ad41fa712e33dbe
maciekflis@gmail.com <maciekflis@gmail.com@7afaae1d-1d65-0f62-2012-5f3b80ce36b9>
2014-10-15 21:19:57 +0000

    Replaced my train wreck code with PagedList implementation Started using StyleCop only to give up in the middle of 1st file, added this to the bottom of my TODO list

--- Prosiak/Areas/Books/Controllers/BooksController.cs
+++ Prosiak/Areas/Books/Controllers/BooksController.cs
@@ -1,127 +1,136 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Prosiak.Areas.Books.Controllers
+﻿namespace Prosiak.Areas.Books.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Net;
+    using System.Web;
+    using System.Web.Mvc;
+    using PagedList;
+
+    /// <summary>
+    /// Controller for Book Area
+    /// </summary>
     public class BooksController : Controller
     {
         private BooksDBContext db = new BooksDBContext();
-        IEnumerable<IEnumerable<Book>> booksGrouped;
+        //private IEnumerable<IEnumerable<Book>> booksGrouped;
+
         // GET: /Books/
         public ActionResult Index(string bookCategory, string searchString, string showNum, int? resPage)
         {
             db.Database.CreateIfNotExists();
-            var CategoryList = new List<string>();
+            var categoryList = new List<string>();
 
-            var CategoryQuery = from d in db.Books
+            var categoryQuery = from d in db.Books
                                 orderby d.Category
                                 select d.Category;
-            CategoryList.AddRange(CategoryQuery.Distinct());
-            ViewBag.bookCategory = new SelectList(CategoryList);
+            categoryList.AddRange(categoryQuery.Distinct());
+            ViewBag.bookCategory = new SelectList(categoryList);
             ViewBag.showNum = new SelectList(new List<string> { "10", "20", "50" });
             ViewBag.selectedShowNum = showNum;
             ViewBag.resPage = resPage != null ? resPage - 1 : 0;
             var books = from b in db.Books
                         where b.Status == true
                         select b;
+
             var booksList = (IEnumerable<Book>)books.ToList();
-
-
 
             if (!String.IsNullOrEmpty(searchString))
             {
                 booksList = booksList.Where(b => b.Title.Contains(searchString) || b.Author.Contains(searchString));
             }
 
-            if (!String.IsNullOrEmpty(bookCategory))
+            if (!(String.IsNullOrEmpty(bookCategory) || bookCategory == "All"))
             {
                 booksList = booksList.Where(b => b.Category == bookCategory);
             }
 
-            if (showNum == "All") //don't group
-            {
-                booksGrouped = (IEnumerable<IEnumerable<Book>>)new List<List<Book>>() { booksList.ToList() };
-            }
-            else if (!string.IsNullOrEmpty(showNum))
-            {
 
-                int groupsize = int.Parse(showNum);
-                //doesnt work with entity
-                //var ZZZ = books.Select((x, i) => new { Index = i, Value = x }).GroupBy(b => (b.Index - 1) / groupsize).Select(g => g.Select(v=>v.Value).ToList()).ToList();
-                //var ZZZ = books.GroupBy((b) => (i - 1) / groupsize).Select(g => g.ToList()).ToList();
-                GroupBooks( ref booksGrouped, booksList, groupsize);
-                //booksGrouped = (IEnumerable<IEnumerable<Book>>)new List<List<Book>>() { books.ToList() };
-            }
-            else
-                booksGrouped = (IEnumerable<IEnumerable<Book>>)new List<List<Book>>() { books.ToList() };
-            return View(booksGrouped);
-        }
-
-        private void GroupBooks(ref IEnumerable<IEnumerable<Book>> booksGrouped, IEnumerable<Book> books, int chunksize)
-        {
-            var books2 = books.ToList();
-            var x = from book in books2
-                    let index = books2.IndexOf(book)
-                    group book by (index + 1) / chunksize into g
-                    select g.AsEnumerable();
-            booksGrouped = x;
+            var pagedBooks = booksList.ToPagedList(resPage ?? 1, int.Parse(showNum ?? "10"));
+            IndexDataContainer model = new IndexDataContainer()
+            {
+                Category = bookCategory,
+                SearchString = searchString,
+                PageNum = resPage ?? 1,
+                ChunkSize = showNum==null ? 10 : int.Parse(showNum),
+                Books = pagedBooks
+            };
+            return View(model);
         }
 
         /// <summary>
         /// Post method for borrowing/returning
         /// </summary>
-        /// <param name="Bookid"></param>
-        /// <param name="Return">1 if book is returned 0 if book is borrowed</param>
+        /// <param name="bookid"></param>
+        /// <param name="returnInd">1 if book is returned 0 if book is borrowed</param>
         /// <returns></returns>
         [ValidateAntiForgeryToken]
         [HttpPost]
-        public ActionResult Index(int? Bookid, int? Return)
+        public ActionResult Index(int? bookid, int? returnInd, string bookCategory, string searchString, string showNum, int? resPage)
         {
             //this is required?
-            var CategoryList = new List<string>();
+            var categoryList = new List<string>();
 
-            var CategoryQuery = from d in db.Books
+            var categoryQuery = from d in db.Books
                                 orderby d.Category
                                 select d.Category;
-            CategoryList.AddRange(CategoryQuery.Distinct());
-            ViewBag.bookCategory = new SelectList(CategoryList);
+            categoryList.AddRange(categoryQuery.Distinct());
+            ViewBag.bookCategory = new SelectList(categoryList);
             ViewBag.showNum = new SelectList(new List<string> { "10", "20", "50" });
             var books = from b in db.Books
                         where b.Status == true
                         select b;
-            
-            if (ModelState.IsValid && Bookid != null && Return != null)
+
+            var booksList = (IEnumerable<Book>)books.ToList();
+
+            if (!String.IsNullOrEmpty(searchString))
             {
+                booksList = booksList.Where(b => b.Title.Contains(searchString) || b.Author.Contains(searchString));
+            }
+
+            if (!(String.IsNullOrEmpty(bookCategory) || bookCategory == "All"))
+            {
+                booksList = booksList.Where(b => b.Category == bookCategory);
+            }
+
+            var pagedBooks = booksList.ToPagedList(resPage ?? 1, int.Parse(showNum ?? "10"));
+            IndexDataContainer model = new IndexDataContainer()
+            {
+                Category = bookCategory,
+                SearchString = searchString,
+                PageNum = resPage ?? 1,
+                ChunkSize = showNum == null ? 10 : int.Parse(showNum),
+                Books = pagedBooks
+            };
 
 
+            if (ModelState.IsValid && bookid != null && returnInd != null)
+            {
                 BookCard bc;
                 Book b = (from d in db.Books
-                          where d.ID == Bookid
+                          where d.ID == bookid
                           select d).First();
 
                 b.Timestamp = DateTime.Now; //should I update this?
                 b.UsrStamp = User.Identity.Name; //-||-
 
-                switch (Return)
+                switch (returnInd)
                 {
-                    case (0)://borrow
+                    case 0://borrow
                         bc = new BookCard
                         {
-                            BookID = (int)Bookid,
+                            BookID = (int)bookid,
                             BorrowDate = DateTime.Now,
                             Reader = User.Identity.Name
                         }; //CAST!!!
                         b.Reader = User.Identity.Name;
                         db.Cards.Add(bc);
                         break;
-                    case (1): //return
+                    case 1: //return
                         bc = (from c in db.Cards
-                              where c.BookID == Bookid && c.ReturnDate == null
+                              where c.BookID == bookid && c.ReturnDate == null
 
                               select c).First();
                         bc.ReturnDate = DateTime.Now;
@@ -130,17 +139,22 @@ namespace Prosiak.Areas.Books.Controllers
                         break;
                 }
 
-                //ReturnDate = DateTime.MinValue 
-
                 db.Entry(b).State = EntityState.Modified;
-                //db.SaveChanges();
-
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", new
+                {
+                    resPage = model.PageNum,
+                    bookCategory = model.Category,
+                    searchString = model.SearchString,
+                    showNum = model.ChunkSize
+                }
+                );
+
                 //return RedirectToAction("Index", new { bookCategory = this.bookCategory, searchString = searchString, showNum = showNum, resPage = resPage });
             }
-
-            return View(books);
+   
+            //below should never ever ever happen?
+            return View(model);
         }
 
         // GET: /Books/Details/5
@@ -150,11 +164,13 @@ namespace Prosiak.Areas.Books.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             Book book = db.Books.Find(id);
             if (book == null)
             {
                 return HttpNotFound();
             }
+
             return View(book);
         }
 
@@ -190,11 +206,13 @@ namespace Prosiak.Areas.Books.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             Book book = db.Books.Find(id);
             if (book == null)
             {
                 return HttpNotFound();
             }
+
             return View(book);
         }
 
@@ -213,6 +231,7 @@ namespace Prosiak.Areas.Books.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
             return View(book);
         }
 
@@ -223,11 +242,13 @@ namespace Prosiak.Areas.Books.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             Book book = db.Books.Find(id);
             if (book == null)
             {
                 return HttpNotFound();
             }
+
             return View(book);
         }
 
@@ -248,6 +269,7 @@ namespace Prosiak.Areas.Books.Controllers
             {
                 db.Dispose();
             }
+
             base.Dispose(disposing);
         }
     }
---
+++ Prosiak/Areas/Books/Models/IndexDataContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prosiak.Areas.Books
+{
+    public class IndexDataContainer
+    {
+        public int PageNum { get; set; }
+        public int ChunkSize { get; set; }
+        public string Category { get; set; }
+        public string SearchString { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
